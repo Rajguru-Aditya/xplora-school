9e142798781b48778c54b4d51df3e0ae784c3da6
Rajguru-Aditya <rajguru.aditya.25@gmail.com>
2024-08-14 00:30:02 +0530

    Change item positions

--- src/pages/online-course/course-detail/index.jsx
+++ src/pages/online-course/course-detail/index.jsx
@@ -10,20 +10,22 @@ import CourseFeeOne from "@/components/CourseFee/CourseFeeOne";
 import CourseHighlights from "@/components/CourseHighlights/CourseHighlights";
 import PlacementGuarantee from "@/components/PlacementGuarantee/PlacementGuarantee";
 import Certificates from "@/components/Certificates/Certificates";
+import InstructorOne from "@/components/Instructor/InstructorOne";
 
 export default function CourseDetails() {
   return (
     <LayoutOne>
       <main>
         <HeroTwo />
-        <ServiceTwo />
-        <CourseTwo />
         <CourseHighlights />
-        {/* <CourseDetailOne /> */}
-        <PartnerTwo />
-        <CourseFeeOne />
+        <ServiceTwo />
+        <InstructorOne />
         <PlacementGuarantee />
+        <PartnerTwo />
         <Certificates />
+        <CourseTwo />
+        {/* <CourseDetailOne /> */}
+        <CourseFeeOne />
         <BlogTwo />
         <CtaOne />
       </main>
