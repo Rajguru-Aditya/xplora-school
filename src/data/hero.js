ad4afd1111d200fa503d36ba66cfb514f7c1aac2
Rajguru-Aditya <rajguru.aditya.25@gmail.com>
2024-08-30 19:03:24 +0530

    Change course page content

--- src/data/hero.js
+++ src/data/hero.js
@@ -157,11 +157,11 @@ const heroUI = {
   title: {
     sliceOne: "UI/UX Design Course",
     sliceTwo: "Courses",
-    sliceThree: "Design Engaging User Experiences",
+    sliceThree: "Create Engaging User Experiences.",
     sliceFour: "Skills",
   },
   description:
-    "A well-designed user experience can make or break a product. Our UI/UX Design course will equip you with the skills to create intuitive and engaging digital interfaces that users love.",
+    "Learn to navigate the fast-paced world of digital marketing with our comprehensive Digital Marketing Mastery course. Designed for beginners and professionals alike, this course covers essential skills such as SEO, SEM, social media marketing, content strategy, email marketing, and data analytics. With hands-on projects and mentorship from industry experts, you'll gain practical experience and build a portfolio that showcases your expertise. Whether you aim to enhance your career or start a new one in digital marketing, this course will equip you with the tools and knowledge to succeed.",
   link: "courses",
   imageOne: HeroTwoImageOne,
   imageTwo: HeroTwoImageTwo,
@@ -181,11 +181,11 @@ const heroMarketing = {
   title: {
     sliceOne: "Digital Marketing Course",
     sliceTwo: "Courses",
-    sliceThree: "Design Engaging User Experiences",
+    sliceThree: "Unlock Your Potential in the Digital World.",
     sliceFour: "Skills",
   },
   description:
-    "Unlock the power of the digital world with our Digital Marketing Mastery course at Xplora School. Designed for high school graduates, working professionals, and career switchers, this comprehensive program will equip you with cutting-edge skills to excel in today’s dynamic marketing landscape",
+    "Master the art of creating user-centered designs with our UI/UX Design Essentials course. This course provides a thorough understanding of the principles of user interface and user experience design, focusing on usability, accessibility, and aesthetics. You’ll learn how to create intuitive and visually appealing digital products through wireframing, prototyping, and user testing. Guided by experienced designers, you'll work on real-world projects that help you build a strong portfolio. Whether you’re looking to start a career in UI/UX or enhance your design skills, this course will empower you to create compelling user experiences.",
   link: "courses",
   imageOne: HeroTwoImageOne,
   imageTwo: HeroTwoImageTwo,
@@ -205,11 +205,11 @@ const heroAnalytics = {
   title: {
     sliceOne: "Data Analytics Course",
     sliceTwo: "Courses",
-    sliceThree: "Design Engaging User Experiences",
+    sliceThree: "Transform Data into Actionable Insights.",
     sliceFour: "Skills",
   },
   description:
-    "Objective: Equip students with the foundational skills and knowledge to perform data analytics, including data manipulation, visualization, statistical analysis, and the basics of machine learning.",
+    "Dive deep into the world of data with our Data Analysis Professional course. This program is perfect for those looking to develop strong analytical skills and learn how to turn raw data into meaningful insights. You'll cover a range of topics including statistical analysis, data visualization, predictive analytics, and data-driven decision-making. Using industry-standard tools and software, you will engage in real-world projects to apply what you’ve learned and solve complex business problems. Prepare to become a data-driven professional ready to impact decision-making processes in any organization.",
   link: "courses",
   imageOne: HeroTwoImageOne,
   imageTwo: HeroTwoImageTwo,
